c6355eda3c40e0815bf71a96daa8a615a6e73f2e
Mirco Nasuti <mirco.nasuti@chuv.ch>
2015-10-13 16:24:31 +0200

    auto-generated project from yeomen -> yo angular (no Sass)

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bootstrapProjectHbpApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the bootstrapProjectHbpApp
+ */
+angular.module('bootstrapProjectHbpApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bootstrapProjectHbpApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bootstrapProjectHbpApp
+ */
+angular.module('bootstrapProjectHbpApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
