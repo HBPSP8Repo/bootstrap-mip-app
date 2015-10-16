7bf58910480e33dd0b2a25babf1004c4897b3210
Mirco Nasuti <mirco.nasuti@chuv.ch>
2015-10-16 16:46:13 +0200

    Do not open browser

--- Gruntfile.js
+++ Gruntfile.js
@@ -95,7 +95,7 @@ module.exports = function (grunt) {
       },
       livereload: {
         options: {
-          open: true,
+          open: false,
           middleware: function (connect) {
             return [
               connect.static('.tmp'),
