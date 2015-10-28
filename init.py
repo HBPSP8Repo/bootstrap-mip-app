91a20f069e3c5bc9300ad05c5b2395742de14646
Mirco Nasuti <mirco.nasuti@chuv.ch>
2015-10-28 09:48:13 +0100

    Bugfix : Check before changing app/index.html in init.py + Feature : display informations and ask for autorun app when running init.py

--- init.py
+++ init.py
@@ -15,6 +15,7 @@ def writeFile(fileName, content):
 		f = open(fileName, 'w')
 		f.write(content)
 		f.close()
+		print 'Changes applied to '+fileName
 	except IOError:
 		print 'writeFile : Cannot write in '+fileName+' file ! '
 
@@ -78,25 +79,32 @@ def main():
 	APP_PATH = os.path.join('app/scripts/app/', APP_ID)
 
 	# Install node and bower dependencies
+	print 'Installing node and bower dependencies...'
 	subprocess.call(['sudo', 'npm', 'install'])
 	subprocess.call(['bower', 'install'])
 
 	# Create application folders
-	subprocess.call(['mkdir', APP_PATH])
-	subprocess.call(['mkdir', os.path.join(APP_PATH, 'css')])
-	subprocess.call(['mkdir', os.path.join(APP_PATH, 'js')])
-	subprocess.call(['mkdir', os.path.join(APP_PATH, 'images')])
+	print 'Creating the folder hierarchy...'
+	subprocess.call(['mkdir', '-v', APP_PATH])
+	subprocess.call(['mkdir', '-v', os.path.join(APP_PATH, 'css')])
+	subprocess.call(['mkdir', '-v', os.path.join(APP_PATH, 'js')])
+	subprocess.call(['mkdir', '-v', os.path.join(APP_PATH, 'images')])
 
 	# Add files
+	print 'Creating some files...'
 	subprocess.call(['touch', os.path.join(APP_PATH, 'index.html')])
 	subprocess.call(['touch', os.path.join(APP_PATH, APP_ID+'.html')])
 	subprocess.call(['touch', os.path.join(APP_PATH, APP_ID+'.module.js')])
 	subprocess.call(['touch', os.path.join(APP_PATH, APP_ID+'.controller.js')])
 	subprocess.call(['touch', os.path.join(APP_PATH, 'css', 'style.css')])
 	subprocess.call(['touch', os.path.join(APP_PATH, 'js', 'script.js')])
-	subprocess.call(['cp', args.logo, os.path.join(APP_PATH, 'images', 'logo.png')])
 
-	# Set up project
+	# Copy files
+	print 'Copying some files...'
+	subprocess.call(['cp', '-v', args.logo, os.path.join(APP_PATH, 'images', 'logo.png')])
+
+	# Set up MVC files
+	print 'Setting up the MVC files...'
 	indexCode = '''
 <div class="container-fluid mt" ng-include=" 'scripts/app/'''+APP_ID+'/'+APP_ID+'''.html' ">
 </div>
@@ -132,9 +140,11 @@ angular.module('chuvApp.'''+APP_ID+'''\').controller(\''''+APP_ID+'''Controller'
 
 
 	# Set up app URL in `Gruntfile.js`
+	print 'Setting up the app URL in the Gruntfile...'
 	replaceContent('./Gruntfile.js', 'target: \'http://localhost:9002/#/hbpapps/myapp\'', 'target: \'http://localhost:9002/#/hbpapps/'+APP_ID+'\'')
 
 	# Add module in `app.js`
+	print 'Adding the app module...'
 	path = './app/scripts/app/app.js'
 	if fileContains(path, '\'chuvApp.'+APP_ID+'\''):
 		print 'The module '+'\'chuvApp.'+APP_ID+'\'seems to already exist in the `app.js` file ! '
@@ -142,18 +152,29 @@ angular.module('chuvApp.'''+APP_ID+'''\').controller(\''''+APP_ID+'''Controller'
 		findAndAppend(path, '//ui modules', '    \'chuvApp.'+APP_ID+'\',\n')	
 
 	# Include module and controller to the main project
+	print 'Including the module and the controller...'
 	path = './app/index.html'
-	inclStr = '\n<!-- JS inclusions for external app "'+APP_ID+'" -->\n'
-	inclStr += '<script src="scripts/app/'+APP_ID+'/'+APP_ID+'.module.js"></script>\n'
-	inclStr += '<script src="scripts/app/'+APP_ID+'/'+APP_ID+'.controller.js"></script>\n'
-	if fileContains(path, inclStr):
+	
+	moduleIncl = '<script src="scripts/app/'+APP_ID+'/'+APP_ID+'.module.js"></script>'
+	controllerIncl = '<script src="scripts/app/'+APP_ID+'/'+APP_ID+'.controller.js"></script>'
+	if fileContains(path, moduleIncl) and fileContains(path, controllerIncl):
 		print 'The Javascript inclusions for this app seem to already exist in the `'+path+'` file ! '
 	else:
+		inclStr = '\n<!-- JS inclusions for external app "'+APP_ID+'" -->\n'
+		inclStr += moduleIncl+'\n'
+		inclStr += controllerIncl+'\n'
 		findAndPrepend(path, '</body>', inclStr)
 
 	# Show welcome message
+	print 'Your app has been automatically created and configured !'
 	print 'To run your application, use `grunt serve` ! '
-
+	choice = ''
+	while choice not in ['y', 'n', 'yes', 'no']:
+		choice = raw_input('Do you want to run it now ?').lower()
+	if choice in ['y', 'yes']:
+		subprocess.call(['grunt', 'serve'])
+	else:
+		print 'Bye ! '
 
 if __name__ == '__main__':
 	main()
